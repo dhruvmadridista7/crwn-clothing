37c4c584d73e013fc1d22b48ce40bbfe84801a1f
DHruv Patel <pateldhruv0077@gmail.com>
2023-08-29 12:52:46 +0530

    added in our addToCart feature

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,15 +1,71 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+
+const addCartItem = (cartItems, productToAdd) => {
+    //find if cartItem contains productToAdd
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === productToAdd.id
+    )
+
+    //If found, increament quantity
+    if(existingCartItem) {
+        return cartItems.map((cartItem) => 
+            cartItem.id === productToAdd.id 
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem
+        );
+    }
+
+    //return new array with modified cartItems/ new cart Items
+    return [ ...cartItems, { ...productToAdd, quantity : 1 }];
+}
 
 export const CartContext = createContext({
     isCartOpen : false,
     setIsCartOpen : () => {},
+    cartItems: [],
+    addItemToCart: () => {},
+    cartCount : 0,
 })
 
 export const CartProvider = ({children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
-    const vale = { isCartOpen, setIsCartOpen };
+    const [cartItems, setCartItems] = useState([]);
+    const [cartCount, setCartCount] = useState(0);
+
+    //instructors approach , but mine one looks batter it's single line code
+    useEffect(() => {
+        const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
+        setCartCount(newCartCount);
+    }, [cartItems])
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd));
+        // setCartCount(cartCount + 1);   //my approach
+    }
+
+    const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount };
 
     return (
-        <CartContext.Provider value={vale}>{children}</CartContext.Provider>
+        <CartContext.Provider value={value}>{children}</CartContext.Provider>
     );
 };
+
+
+
+/*
+product
+{
+    id,
+    name,
+    price,
+    imageUrl
+}
+Cart Item
+{
+    id,
+    name,
+    price,
+    imageUrl,
+    quantity
+}
+*/
